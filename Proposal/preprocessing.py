35d252d2a00c0501798fee335548a132e44a9670
Morten <morten.dahl@gmx.net>
2019-04-17 13:34:43 +0100

    DT & Centroid Undersampling

--- Proposal/preprocessing.py
+++ Proposal/preprocessing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import  OneHotEncoder, LabelEncoder
+import numpy as np
+from math import sqrt
 
 def convert_to_boolean(df):
     '''
@@ -55,3 +57,33 @@ def encode_days_as_costumer(df):
     ).dt.days
     return dataframe
 
+
+def centroid_undersampling(X, f):
+    """
+       Implementation of CCMUT (cluster-centroid based Majority under-sampling technique)
+       :param X: the df
+       :param f: % of undersampling
+       :return: X_f a dataframe with undersampled data for both labels
+       """
+    # get subset of X with label
+    X_label = X.loc[X["Response"]==0, :]
+
+    # initialize a new columns
+    X_label["distance"] = 0
+
+    # getting cluster-centroids
+    cluster_centroid = np.sum(X_label, axis=0) / X_label.shape[0]
+
+    # get the euclidean distance between each sample and the centroid -> sum it afterwards
+    for index, row in X_label.iterrows():
+        X_label.loc[index, "distance"] = sqrt(sum((cluster_centroid - X_label.loc[index, :]) ** 2))
+
+    # select the highest distances and drop the distances
+    X_label.sort_values(by=['distance'], ascending=False, inplace=True)
+    X_label.drop("distance", axis=1, inplace=True)
+
+    # concatenate all selected 0 labels with the 1 labels
+    X_f = pd.concat([X_label.iloc[:int(f * X_label.shape[0]), :], X.loc[X["Response"]==1, :]])
+    # shuffle the dataframe and reset the index
+    X_f = X_f.sample(frac=1).reset_index(drop=True)
+    return X_f
