f3c5f975db6fef591d57888b9be126d6aca18166
Antonio Curado <1140280@isep.ipp.pt>
2019-04-27 20:31:00 +0100

    Changed kears weight reset

--- Proposal/ML_algorithms.py
+++ Proposal/ML_algorithms.py
@@ -51,11 +51,16 @@ def KerasNN(X_train, y_train, input_dim=32, n_layers=4, optimizer="rmsprop", los
     model.fit(X_train, y_train, epochs=100, verbose=0)
     return model
 
-def KerasNN_not_fitted(input_dim=32, n_layers=4, n_neurons=6, r_dropout=0.5, optimizer="rmsprop", loss="binary_crossentropy", init="uniform", metrics=["accuracy"], random_state=42):
+def KerasNN_not_fitted(input_dim=39, n_layers=4, n_neurons=6, r_dropout=0.5, optimizer="rmsprop", loss="binary_crossentropy", init="uniform", metrics=["accuracy"], random_state=42):
     """
     Keras Neural Network, define the amount of layers you want, which optimizer you want to use and which loss function you want to apply.
     """ 
     np.random.seed(random_state)
+    
+
+    from keras import backend as K
+    K.clear_session()
+
 
     model = models.Sequential()
     model.add(layers.Dense(n_neurons, activation="relu", input_dim=input_dim))
@@ -66,9 +71,6 @@ def KerasNN_not_fitted(input_dim=32, n_layers=4, n_neurons=6, r_dropout=0.5, opt
     model.add(layers.Dense(1, activation="sigmoid", init=init))
     model.compile(optimizer, loss, metrics=metrics)
     
-    initial_weights = model.get_weights()
-    
-    utils.shuffle_weights(model, initial_weights)
     return model
 
 def Gaussian_NB(X_train, y_train):
