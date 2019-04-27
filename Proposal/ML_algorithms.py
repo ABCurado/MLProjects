18e72adcb0b4ddc622c21e9650426cc1c56c7caf
Joris ` <jorisanton.bertens@gmail.com>
2019-04-27 02:35:13 +0100

    Nothing Special

--- Proposal/ML_algorithms.py
+++ Proposal/ML_algorithms.py
@@ -51,16 +51,18 @@ def KerasNN(X_train, y_train, input_dim=32, n_layers=4, optimizer="rmsprop", los
     model.fit(X_train, y_train, epochs=100, verbose=0)
     return model
 
-def KerasNN_not_fitted(input_dim=32, n_layers=4, optimizer="rmsprop", loss="binary_crossentropy", init="uniform", metrics=["accuracy"], random_state=42):
+def KerasNN_not_fitted(input_dim=32, n_layers=4, n_neurons=6, r_dropout=0.5, optimizer="rmsprop", loss="binary_crossentropy", init="uniform", metrics=["accuracy"], random_state=42):
     """
     Keras Neural Network, define the amount of layers you want, which optimizer you want to use and which loss function you want to apply.
     """ 
     np.random.seed(random_state)
 
     model = models.Sequential()
-    model.add(layers.Dense(6, activation="relu", input_dim=input_dim))
+    model.add(layers.Dense(n_neurons, activation="relu", input_dim=input_dim))
+    model.add(layers.Dropout(r_dropout))
     for num in range(n_layers-2):
-        model.add(layers.Dense(6, activation="relu"))
+        model.add(layers.Dense(n_neurons, activation="relu"))
+        model.add(layers.Dropout(r_dropout))
     model.add(layers.Dense(1, activation="sigmoid", init=init))
     model.compile(optimizer, loss, metrics=metrics)
     
