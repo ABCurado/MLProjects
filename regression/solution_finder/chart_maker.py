1aedb35d4280d5b3c50a5de36c1428a898e2efdc
Antonio Curado <1140280@isep.ipp.pt>
2019-05-30 14:28:14 +0100

    Added chart maker

---
+++ regression/solution_finder/chart_maker.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import pandas as pd
+from itertools import cycle
+# Color Cycle
+cycol = cycle('bgkyrcm')
+
+df = pd.read_csv('../log_files/operator_probs_14_9_log.csv')
+x = df.index
+operator_lines = []
+fig, ax = plt.subplots()
+
+for i, line in enumerate(df):
+    operator_line, = ax.plot(x, df[df.columns[i]], color=next(cycol))
+    operator_lines.append(operator_line)
+
+
+def init():
+    for line in operator_lines:
+        line.set_data([],[])
+    return operator_lines
+
+plt.xlabel('Percentage')
+plt.ylabel('Generation')
+plt.gca().legend(set(df.columns))
+def update(num, x, lines):
+    for i, operator_line in enumerate(lines):
+        operator_line.set_data(x[:num], df[df.columns[i]][:num])
+    return lines
+
+ani = animation.FuncAnimation(fig, update,init_func=init, fargs=[x, operator_lines],
+                              interval=1, blit=True)
+ani.save('operator_percentages.gif', writer='imagemagick')
+plt.show()
