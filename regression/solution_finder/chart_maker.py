de54d516e53c3b6c2c32b004502108d0c9c23590
Antonio Curado <1140280@isep.ipp.pt>
2019-05-31 19:08:18 +0100

    Bug fixes 4

--- regression/solution_finder/chart_maker.py
+++ regression/solution_finder/chart_maker.py
@@ -5,12 +5,14 @@ import pandas as pd
 from itertools import cycle
 # Color Cycle
 cycol = cycle('bgkyrcm')
+df = pd.read_csv('../log_files/fitness_&_operator_probs_19_1_log.csv')
 
-df = pd.read_csv('../log_files/fitness_&_operator_probs_21_28_log.csv')
-df = df.drop(["elite_fitness", "generation", "population_fitness"], axis=1)
+df = df.drop(["elite_fitness", "generation", "population_fitness", "val_fitness"], axis=1)
 x = df.index
 operator_lines = []
 fig, ax = plt.subplots()
+
+
 
 for i, line in enumerate(df):
     operator_line, = ax.plot(x, df[df.columns[i]], color=next(cycol))
@@ -21,9 +23,11 @@ def init():
     for line in operator_lines:
         line.set_data([],[])
     return operator_lines
+for column in df.columns:
+    print(df[column].describe())
 
-plt.xlabel('Percentage')
-plt.ylabel('Generation')
+plt.xlabel('Generation')
+plt.ylabel('Percentage')
 plt.gca().legend(set(df.columns))
 def update(num, x, lines):
     for i, operator_line in enumerate(lines):
